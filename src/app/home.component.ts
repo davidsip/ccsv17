1872823513e48d8820ef88ecc76387fd79cfd135
davidsip <davidsip@outlook.com>
2017-10-08 21:50:00 -0700

    new index stuff

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CarouselComponent } from './carousel.component';
-
+import { Router } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { AppComponent } from './app.component';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +12,21 @@ import { CarouselComponent } from './carousel.component';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+      private router: Router,
+      private route: ActivatedRoute,
+private location: Location
+  ) { }
 
   ngOnInit() {
   }
+
+  gotoEvent(){
+    this.router.navigateByUrl('/events');
+
+  }
+  goBack(): void {
+  this.location.back();
+}
 
 }
