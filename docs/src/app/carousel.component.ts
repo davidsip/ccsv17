c14e4c90a24a47f7ba390b8c4cf1b1261a34b914
davidsip <davidsip@outlook.com>
2017-10-08 23:58:31 -0700

    commented out 1

--- docs/src/app/carousel.component.ts
+++ docs/src/app/carousel.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Image } from './image.interface';
+// import { Image } from './image.interface';
 
 @Component({
   selector: 'app-carousel',
